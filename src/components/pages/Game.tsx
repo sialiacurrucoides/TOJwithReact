98de681b3bd773128fa7479c10f0dcb9945de3d1
Simon Júlia <simon.julia@nexogen.hu>
2021-06-16 22:12:53 +0200

    game page markup

--- src/components/pages/Game.tsx
+++ src/components/pages/Game.tsx
@@ -1,6 +1,35 @@
+import { useState } from 'react';
+import styles from './Game.module.scss';
+import balloonImg from '../../assets/imgs/juli_jatek_lufi.png';
+import balloonGoImg from '../../assets/imgs/juli_jatek_lufi_GO.png'; 
+
 const Game: React.FC = () => {
+    const [ballonLives, setBallonLives] = useState([1,2,3,4,5,6,7,8]);
+
     return (
-        <div>Game</div>
+        <div className={styles.gameField}>
+            <div className={styles.startText}><span>Click GO!</span></div>
+            <div className={styles.balloon}>
+                <img src={balloonGoImg} alt="balloon to levitate"/>
+            </div>
+            <div className={styles.feedback}>
+                <div className={styles.leftArrow}>
+                    <span className={styles.arrow}> &larr; </span>
+                </div>
+                <div className={styles.balloonLives}>
+                    {ballonLives.map(miniBalloonNr => <img 
+                    width="20px"
+                    height="20px"
+                    key={miniBalloonNr} 
+                    src={balloonImg}
+                    alt={`ballon${miniBalloonNr}`}
+                     />)}
+                </div>
+                <div className={styles.rightArrow}>
+                    <span className={styles.arrow}> &rarr; </span>
+                </div>
+            </div>
+        </div>
     );
 };
 
